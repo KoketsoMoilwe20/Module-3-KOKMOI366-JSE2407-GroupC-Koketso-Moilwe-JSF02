37ed8e24292fa44ad14571be440c68e0178151c7
Koketso Moilwe <koketsomoilwe2@gmail.com>
2024-07-29 21:17:52 +0200

    Start over :(

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,1 @@
-<script>
-  import {Router, Link, Route} from "svelte-routing";
 
-  import Header from "./Components/Header.svelte";
-</script>
-
-<style lang="postcss">
-  :global(html) {
-      background-color: #caf0f8;
-  }
-
-</style>
-
-<Header />
