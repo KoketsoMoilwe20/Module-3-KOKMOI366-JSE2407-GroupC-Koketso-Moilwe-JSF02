c7442202e9991772110d1ab6fd3b8468d0155920
Koketso Moilwe <koketsomoilwe2@gmail.com>
2024-07-29 22:16:47 +0200

    importation

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,10 @@
     import {Router, Link, Route} from "svelte-routing"
     import Header from "./Components/Header.svelte";
     import ProductList from "./Components/Products/ProductList.svelte";
+    import {selectedCategory, searchQuery, sortOption} from "./Components/ProductStore/store";
+    import {onMount} from "svelte";
+
+    
 </script>
 
 <style>
