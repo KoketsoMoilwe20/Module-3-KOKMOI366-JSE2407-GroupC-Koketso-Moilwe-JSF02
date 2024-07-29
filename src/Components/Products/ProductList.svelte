d1d615bdd7399c296410d03a9ddc18365542f1f6
Koketso Moilwe <koketsomoilwe2@gmail.com>
2024-07-29 22:02:05 +0200

    import state management variables

---
+++ src/Components/Products/ProductList.svelte
@@ -0,0 +1,17 @@
+<script>
+
+    import {selectedCategory, searchQuery, sortOption} from "../ProductStore/store"
+    
+</script>
+
+<style>
+
+</style>
+
+<div class="filters-and-sort">
+
+    <div class="filters">
+
+    </div>
+
+</div>
