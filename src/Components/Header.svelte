74b868d48e08a9ed86fb9377cbfcde79deb7e040
Koketso Moilwe <koketsomoilwe2@gmail.com>
2024-07-30 14:35:05 +0200

    Fix routing

--- src/Components/Header.svelte
+++ src/Components/Header.svelte
@@ -70,8 +70,14 @@
   
   <header>
     <div class="logo">
-      <img src="../online-shop.png" alt="Logo" />
-      <h1>SwiftCart</h1>
+        <Link to="/">
+            <button class="flex items-center space-x-3 rtl:space-x-reverse">
+              <img src="/online-shop.png" class="h-8" alt="Flowbite Logo" />
+              <span class="self-center text-2xl font-semibold whitespace-nowrap text-white">
+                SwiftCart
+              </span>
+            </button>
+          </Link>
     </div>
     <div class="hamburger" on:click={toggleMenu}>
       <span></span>
@@ -79,9 +85,34 @@
       <span></span>
     </div>
     <nav class:open={isMenuOpen}>
-      <Link to="/">Home</Link>
+      
       <Link to="/wishlist">Wishlist</Link>
-      <Link to="/cart">Cart</Link>
+      <Link to="">
+        <li class="relative flex items-center">
+          <!-- Notification Badge -->
+          <div class="absolute left-3 -top-4 flex items-center justify-center">
+            <p class="flex h-4 w-4 items-center justify-center rounded-full bg-red-500 text-xs text-white">
+              2
+            </p>
+          </div>
+          <!-- Icon -->
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="h-6 w-6 stroke-white cursor-pointer"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+            />
+          </svg>
+        </li>
+      </Link>
+      
       <Link to="/login">Login</Link>
     </nav>
   </header>
