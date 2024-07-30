94a2f82c50b686882ddfa3407aa0ce6d6058237a
Koketso Moilwe <koketsomoilwe2@gmail.com>
2024-07-30 09:02:09 +0200

    Update Header styling

--- src/Components/Header.svelte
+++ src/Components/Header.svelte
@@ -1,22 +1,29 @@
 <script>
-
-</script>
-
-<style>
+    import { Link } from "svelte-routing";
+    let isMenuOpen = false;
+  
+    function toggleMenu() {
+      isMenuOpen = !isMenuOpen;
+    }
+  </script>
+  
+  <style>
     header {
-        background-color: #145858;
-        padding: 1rem;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
+      background-color: #145858;
+      padding: 1rem;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
     }
-
     .logo {
-        display: flex;
-        align-items: center;
-        color: white;
+      display: flex;
+      align-items: center;
+      color: white;
     }
-
+    .logo img {
+      height: 2rem;
+      margin-right: 1rem;
+    }
     nav {
       display: flex;
       gap: 2rem;
@@ -28,18 +35,53 @@
     .menu a:hover {
       text-decoration: underline;
     }
-</style>
-
-<header>
+    .hamburger {
+      display: none;
+      flex-direction: column;
+      cursor: pointer;
+    }
+    .hamburger span {
+      height: 2px;
+      width: 25px;
+      background: white;
+      margin-bottom: 4px;
+      border-radius: 5px;
+    }
+  
+    @media (max-width: 768px) {
+      .hamburger {
+        display: flex;
+      }
+      nav {
+        display: none;
+        flex-direction: column;
+        position: absolute;
+        top: 60px;
+        left: 0;
+        right: 0;
+        background-color: #145858;
+        padding: 1rem;
+      }
+      nav.open {
+        display: flex;
+      }
+    }
+  </style>
+  
+  <header>
     <div class="logo">
-        <img src="../assets/online-shop.png" />
-        <h1>SwiftCart</h1>
+      <img src="../assets/online-shop.png" alt="Logo" />
+      <h1>SwiftCart</h1>
     </div>
-
-    <nav class="menu">
-        <a href="#">Wishlist</a>
-        <a href="#">Cart</a>
-        <a href="#">Login</a>
+    <div class="hamburger" on:click={toggleMenu}>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+    <nav class:open={isMenuOpen}>
+      <Link to="/">Home</Link>
+      <Link to="/wishlist">Wishlist</Link>
+      <Link to="/cart">Cart</Link>
+      <Link to="/login">Login</Link>
     </nav>
-
-</header>
+  </header>
