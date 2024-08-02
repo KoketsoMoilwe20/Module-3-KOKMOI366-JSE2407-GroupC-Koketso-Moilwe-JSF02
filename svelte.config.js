3dfef2f26a6c1db5a9c279e79c46f4fbcb246d31
Koketso Moilwe <koketsomoilwe2@gmail.com>
2024-08-02 14:55:20 +0200

    installing svelte-preprocess

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,12 @@
 import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { sveltePreprocess } from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: adapter()
   },
-  preprocess: vitePreprocess()
+  preprocess: sveltePreprocess()
 };
 
 export default config;
